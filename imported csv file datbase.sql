e6bef08cb86d950d3822e9d2fe8bcd56df309950
Uwadia O. Kelly <146957698+rukkyvibe02@users.noreply.github.com>
2023-10-08 01:52:31 +0100

    Add files via upload

---
+++ imported csv file datbase.sql
@@ -0,0 +1,12 @@
+-- create table nba (
+--    Name VARCHAR(64),
+-- 	Team VARCHAR(150),
+--  	Position VARCHAR(64),
+--  	Birthday VARCHAR(64),
+--   	Salary VARCHAR(50)	
+--   	);
+	
+-- INSERT INTO nba (Name,Team,Position,Birthday,Salary) values('Shake Milton','Philadelphia 76ers','SG','1996-09-26','1445697');
+-- INSERT INTO nba (Name,Team,Position,Birthday,Salary) values('Christian Wood','Detroit Pistons','PF','1995-09-27','1645357');
+ select * from nba;
+-- DROP TABLE nba;
